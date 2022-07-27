435b9cfba175138c6df6b86d9f6596d938c29c0e
JYuan <13826506407@139.com>
2022-07-27 17:02:42 +0800

    设置tiles阴影

--- example/b3dmExample.js
+++ example/b3dmExample.js
@@ -1,4 +1,4 @@
-import { B3DMLoader } from '../src/index.js';
+import { B3DMLoader } from "../src/index.js";
 import {
 	Scene,
 	Group,
@@ -15,8 +15,8 @@ import {
 	UniformsUtils,
 	ShaderMaterial,
 	Color,
-} from 'three';
-import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
+} from "three";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
 
 let camera, controls, scene, renderer, offsetGroup;
 let dirLight;
@@ -28,13 +28,12 @@ init();
 animate();
 
 // Adjusts the three.js standard shader to include batchid highlight
-function batchIdHighlightShaderMixin( shader ) {
-
+function batchIdHighlightShaderMixin(shader) {
 	const newShader = { ...shader };
 	newShader.uniforms = {
-		highlightedBatchId: { value: - 1 },
-		highlightColor: { value: new Color( 0xFFC107 ).convertSRGBToLinear() },
-		...UniformsUtils.clone( shader.uniforms ),
+		highlightedBatchId: { value: -1 },
+		highlightColor: { value: new Color(0xffc107).convertSRGBToLinear() },
+		...UniformsUtils.clone(shader.uniforms),
 	};
 	newShader.extensions = {
 		derivatives: true,
@@ -69,175 +68,158 @@ function batchIdHighlightShaderMixin( shader ) {
 		);
 
 	return newShader;
-
 }
 
 function init() {
-
-	infoEl = document.getElementById( 'info' );
+	infoEl = document.getElementById("info");
 
 	scene = new Scene();
 
 	// primary camera view
-	renderer = new WebGLRenderer( { antialias: true } );
-	renderer.setPixelRatio( window.devicePixelRatio );
-	renderer.setSize( window.innerWidth, window.innerHeight );
-	renderer.setClearColor( 0x151c1f );
+	renderer = new WebGLRenderer({ antialias: true });
+	renderer.setPixelRatio(window.devicePixelRatio);
+	renderer.setSize(window.innerWidth, window.innerHeight);
+	renderer.setClearColor(0x151c1f);
 	renderer.shadowMap.enabled = true;
 	renderer.shadowMap.type = PCFSoftShadowMap;
 	renderer.outputEncoding = sRGBEncoding;
 
-	document.body.appendChild( renderer.domElement );
+	document.body.appendChild(renderer.domElement);
 
-	camera = new PerspectiveCamera( 60, window.innerWidth / window.innerHeight, 1, 4000 );
-	camera.position.set( 400, 400, 400 );
+	camera = new PerspectiveCamera(
+		60,
+		window.innerWidth / window.innerHeight,
+		1,
+		4000
+	);
+	camera.position.set(400, 400, 400);
 
 	// controls
-	controls = new OrbitControls( camera, renderer.domElement );
+	controls = new OrbitControls(camera, renderer.domElement);
 	controls.screenSpacePanning = false;
 	controls.minDistance = 1;
 	controls.maxDistance = 2000;
 
 	// lights
-	dirLight = new DirectionalLight( 0xffffff, 1.25 );
-	dirLight.position.set( 1, 2, 3 ).multiplyScalar( 40 );
+	dirLight = new DirectionalLight(0xffffff, 1.25);
+	dirLight.position.set(1, 2, 3).multiplyScalar(40);
 	dirLight.castShadow = true;
-	dirLight.shadow.bias = - 0.01;
-	dirLight.shadow.mapSize.setScalar( 2048 );
+	dirLight.shadow.bias = -0.01;
+	dirLight.shadow.mapSize.setScalar(2048);
 
 	const shadowCam = dirLight.shadow.camera;
-	shadowCam.left = - 200;
-	shadowCam.bottom = - 200;
+	shadowCam.left = -200;
+	shadowCam.bottom = -200;
 	shadowCam.right = 200;
 	shadowCam.top = 200;
 	shadowCam.updateProjectionMatrix();
 
-	scene.add( dirLight );
+	scene.add(dirLight);
 
-	const ambLight = new AmbientLight( 0xffffff, 0.05 );
-	scene.add( ambLight );
+	const ambLight = new AmbientLight(0xffffff, 0.05);
+	scene.add(ambLight);
 
 	offsetGroup = new Group();
-	scene.add( offsetGroup );
+	scene.add(offsetGroup);
 
 	new B3DMLoader()
-		.load( 'https://raw.githubusercontent.com/CesiumGS/3d-tiles-samples/main/1.0/TilesetWithRequestVolume/city/lr.b3dm' )
-		.then( res => {
-
-			console.log( res );
+		.load(
+			"https://raw.githubusercontent.com/CesiumGS/3d-tiles-samples/main/1.0/TilesetWithRequestVolume/city/lr.b3dm"
+		)
+		.then((res) => {
+			console.log(res);
 			model = res.scene;
-			offsetGroup.add( model );
+			offsetGroup.add(model);
 
 			const box = new Box3();
-			box.setFromObject( model );
-			box.getCenter( offsetGroup.position ).multiplyScalar( - 1 );
-
+			box.setFromObject(model);
+			box.getCenter(offsetGroup.position).multiplyScalar(-1);
 
 			// reassign the material to use the batchid highlight variant.
 			// in practice this should copy over any needed uniforms from the
 			// original material.
-			model.traverse( c => {
-
-				if ( c.isMesh ) {
-
-					c.material = new ShaderMaterial( batchIdHighlightShaderMixin( ShaderLib.standard ) );
-
+			model.traverse((c) => {
+				if (c.isMesh) {
+					c.material = new ShaderMaterial(
+						batchIdHighlightShaderMixin(ShaderLib.standard)
+					);
 				}
-
-			} );
-
-		} );
+			});
+		});
 
 	raycaster = new Raycaster();
 	mouse = new Vector2();
 
 	onWindowResize();
-	window.addEventListener( 'resize', onWindowResize, false );
-	renderer.domElement.addEventListener( 'mousemove', onMouseMove, false );
-
+	window.addEventListener("resize", onWindowResize, false);
+	renderer.domElement.addEventListener("mousemove", onMouseMove, false);
 }
 
-function onMouseMove( e ) {
-
+function onMouseMove(e) {
 	const bounds = this.getBoundingClientRect();
 	mouse.x = e.clientX - bounds.x;
 	mouse.y = e.clientY - bounds.y;
-	mouse.x = ( mouse.x / bounds.width ) * 2 - 1;
-	mouse.y = - ( mouse.y / bounds.height ) * 2 + 1;
+	mouse.x = (mouse.x / bounds.width) * 2 - 1;
+	mouse.y = -(mouse.y / bounds.height) * 2 + 1;
 
-	raycaster.setFromCamera( mouse, camera );
+	raycaster.setFromCamera(mouse, camera);
 
 	// Get the batch table data
-	const intersects = raycaster.intersectObject( scene, true );
-	let hoveredBatchid = - 1;
-	if ( intersects.length ) {
+	const intersects = raycaster.intersectObject(scene, true);
+	let hoveredBatchid = -1;
+	if (intersects.length) {
+		const { face, object } = intersects[0];
+		const batchidAttr = object.geometry.getAttribute("_batchid");
 
-		const { face, object } = intersects[ 0 ];
-		const batchidAttr = object.geometry.getAttribute( '_batchid' );
-
-		if ( batchidAttr ) {
-
+		if (batchidAttr) {
 			// Traverse the parents to find the batch table.
 			let batchTableObject = object;
-			while ( ! batchTableObject.batchTable ) {
-
+			while (!batchTableObject.batchTable) {
 				batchTableObject = batchTableObject.parent;
-
 			}
 
 			// Log the batch data
 			const batchTable = batchTableObject.batchTable;
-			hoveredBatchid = batchidAttr.getX( face.a );
+			hoveredBatchid = batchidAttr.getX(face.a);
 
 			infoEl.innerText =
-				`_batchid   : ${ hoveredBatchid }\n` +
-				`Latitude   : ${ batchTable.getData( 'Latitude' )[ hoveredBatchid ].toFixed( 3 ) }\n` +
-				`Longitude  : ${ batchTable.getData( 'Longitude' )[ hoveredBatchid ].toFixed( 3 ) }\n` +
-				`Height     : ${ batchTable.getData( 'Height' )[ hoveredBatchid ].toFixed( 3 ) }\n`;
-
+				`_batchid   : ${hoveredBatchid}\n` +
+				`Latitude   : ${batchTable
+					.getData("Latitude")
+					[hoveredBatchid].toFixed(3)}\n` +
+				`Longitude  : ${batchTable
+					.getData("Longitude")
+					[hoveredBatchid].toFixed(3)}\n` +
+				`Height     : ${batchTable
+					.getData("Height")
+					[hoveredBatchid].toFixed(3)}\n`;
 		}
-
 	} else {
-
-		infoEl.innerText = '';
-
+		infoEl.innerText = "";
 	}
 
-	if ( model ) {
-
-		model.traverse( c => {
-
-			if ( c.isMesh ) {
-
+	if (model) {
+		model.traverse((c) => {
+			if (c.isMesh) {
 				c.material.uniforms.highlightedBatchId.value = hoveredBatchid;
-
 			}
-
-		} );
-
+		});
 	}
-
 }
 
 function onWindowResize() {
-
 	camera.aspect = window.innerWidth / window.innerHeight;
-	renderer.setPixelRatio( window.devicePixelRatio );
-	renderer.setSize( window.innerWidth, window.innerHeight );
+	renderer.setPixelRatio(window.devicePixelRatio);
+	renderer.setSize(window.innerWidth, window.innerHeight);
 	camera.updateProjectionMatrix();
-
 }
 
 function animate() {
-
-	requestAnimationFrame( animate );
+	requestAnimationFrame(animate);
 
 	render();
-
 }
 
 function render() {
-
-	renderer.render( scene, camera );
-
+	renderer.render(scene, camera);
 }
--- example/gltf.js
+++ example/gltf.js
@@ -1,4 +1,4 @@
-import { GLTFExtensionLoader } from '../src/three/GLTFExtensionLoader.js';
+import { GLTFExtensionLoader } from "../src/three/GLTFExtensionLoader.js";
 import {
 	Scene,
 	DirectionalLight,
@@ -7,11 +7,17 @@ import {
 	PerspectiveCamera,
 	sRGBEncoding,
 	PCFSoftShadowMap,
-} from 'three';
-import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
-import { LoadingManager } from 'three';
-import { DRACOLoader } from 'three/examples/jsm/loaders/DRACOLoader';
-import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader';
+	CameraHelper,
+	PlaneGeometry,
+	MeshStandardMaterial,
+	Mesh,
+	DoubleSide,
+	TorusKnotGeometry,
+} from "three";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
+import { LoadingManager } from "three";
+import { DRACOLoader } from "three/examples/jsm/loaders/DRACOLoader";
+import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
 
 let camera, controls, scene, renderer;
 let dirLight;
@@ -19,128 +25,166 @@ let dirLight;
 init();
 animate();
 
-function init() {
+// 已学习
 
+function init() {
 	scene = new Scene();
 
 	// primary camera view
-	renderer = new WebGLRenderer( { antialias: true } );
-	renderer.setPixelRatio( window.devicePixelRatio );
-	renderer.setSize( window.innerWidth, window.innerHeight );
-	renderer.setClearColor( 0x151c1f );
+	renderer = new WebGLRenderer({ antialias: true });
+	renderer.setPixelRatio(window.devicePixelRatio);
+	renderer.setSize(window.innerWidth, window.innerHeight);
+	renderer.setClearColor(0x151c1f);
 	renderer.shadowMap.enabled = true;
 	renderer.shadowMap.type = PCFSoftShadowMap;
 	renderer.outputEncoding = sRGBEncoding;
 
-	document.body.appendChild( renderer.domElement );
+	document.body.appendChild(renderer.domElement);
 
-	camera = new PerspectiveCamera( 60, window.innerWidth / window.innerHeight, 1, 4000 );
-	camera.position.set( 3, 10, 20 );
+	camera = new PerspectiveCamera(
+		60,
+		window.innerWidth / window.innerHeight,
+		1,
+		4000
+	);
+	camera.position.set(0, 10, 20);
 
 	// controls
-	controls = new OrbitControls( camera, renderer.domElement );
+	controls = new OrbitControls(camera, renderer.domElement);
 	controls.screenSpacePanning = false;
 	controls.minDistance = 1;
 	controls.maxDistance = 2000;
 
 	// lights
-	dirLight = new DirectionalLight( 0xffffff, 1.25 );
-	dirLight.position.set( 1, 2, 3 ).multiplyScalar( 40 );
+	dirLight = new DirectionalLight(0xffffff, 1.25);
+	dirLight.position.set(1, 2, 3).multiplyScalar(40);
 	dirLight.castShadow = true;
-	dirLight.shadow.bias = - 0.01;
-	dirLight.shadow.mapSize.setScalar( 2048 );
+	dirLight.shadow.bias = -0.01;
+	dirLight.shadow.mapSize.setScalar(2048);
 
 	const shadowCam = dirLight.shadow.camera;
-	shadowCam.left = - 200;
-	shadowCam.bottom = - 200;
+	shadowCam.left = -200;
+	shadowCam.bottom = -200;
 	shadowCam.right = 200;
 	shadowCam.top = 200;
 	shadowCam.updateProjectionMatrix();
-	scene.add( dirLight );
+	scene.add(dirLight);
 
-	const ambLight = new AmbientLight( 0xffffff, 0.05 );
-	scene.add( ambLight );
+	let shadowCamHelper = new CameraHelper(dirLight.shadow.camera);
+	shadowCamHelper.name = `dirLight_helper`;
+	scene.add(shadowCamHelper);
 
-	// basic gltf test files
+	const ambLight = new AmbientLight(0xffffff, 0.05);
+	scene.add(ambLight);
+
+	// 平面
+	const geometry = new PlaneGeometry(100, 100);
+	const material = new MeshStandardMaterial({
+		color: 0xefe7b0,
+		metalness: 0.0,
+		roughness: 1.0,
+		side: DoubleSide,
+	});
+	const ground = new Mesh(geometry, material);
+	ground.position.set(0, -10, 0);
+	ground.rotation.set(Math.PI / 2, 0, 0);
+	ground.castShadow = false;
+	ground.receiveShadow = true;
+	scene.add(ground);
+
+	// 参照物
+	let torusKnotGGeometry = new TorusKnotGeometry(25, 8, 75, 20);
+	let torusKnotMaterial = new MeshStandardMaterial({
+		color: 0xff0000,
+		metalness: 0.9,
+		roughness: 0.1,
+	});
+	const torusKnot3D = new Mesh(torusKnotGGeometry, torusKnotMaterial);
+	torusKnot3D.position.set(0, 0, 0);
+	torusKnot3D.scale.set(0.05, 0.05, 0.05);
+	torusKnot3D.castShadow = true;
+	torusKnot3D.receiveShadow = true;
+	scene.add(torusKnot3D);
+
 	let insertPosition = 0;
+	// 基本gltf测试文件
 	const gltfModelTests = [
-		'https://raw.githubusercontent.com/KhronosGroup/glTF-Sample-Models/master/2.0/CesiumMilkTruck/glTF-Binary/CesiumMilkTruck.glb',
-		'https://raw.githubusercontent.com/KhronosGroup/glTF-Sample-Models/master/2.0/CesiumMilkTruck/glTF-Embedded/CesiumMilkTruck.gltf',
-		'https://raw.githubusercontent.com/KhronosGroup/glTF-Sample-Models/master/2.0/CesiumMilkTruck/glTF/CesiumMilkTruck.gltf',
+		"https://raw.githubusercontent.com/KhronosGroup/glTF-Sample-Models/master/2.0/CesiumMilkTruck/glTF-Binary/CesiumMilkTruck.glb",
+		"https://raw.githubusercontent.com/KhronosGroup/glTF-Sample-Models/master/2.0/CesiumMilkTruck/glTF-Embedded/CesiumMilkTruck.gltf",
+		"https://raw.githubusercontent.com/KhronosGroup/glTF-Sample-Models/master/2.0/CesiumMilkTruck/glTF/CesiumMilkTruck.gltf",
 	];
 
-	for ( const url of gltfModelTests ) {
-
+	for (const url of gltfModelTests) {
 		const loader = new GLTFExtensionLoader();
-		loader.workingPath = loader.workingPathForURL( url );
-		loader.load( url )
-			.then( res => {
-
-				res.scene.position.set( insertPosition += 5, 0, 0 );
-				controls.target.set( insertPosition / 2, 0, 0 );
-				controls.update();
-				console.log( 'default loader:', { gltf: res, url } );
-				scene.add( res.scene );
-
-			} );
-
+		loader.workingPath = loader.workingPathForURL(url);
+		loader.load(url).then((res) => {
+			res.scene.position.set((insertPosition += 5), 0, 0);
+			controls.target.set(insertPosition / 2, 0, 0);
+			controls.update();
+			res.scene.traverse((node) => {
+				if (node.type === "Mesh") {
+					// console.log("[node]", url, node);
+					node.castShadow = true;
+					node.receiveShadow = true;
+				}
+			});
+			// console.log("default loader:", { gltf: res, url });
+			scene.add(res.scene);
+		});
 	}
 
-	// gltf with extensions
+	// gltf扩展
 	const delegatedLoaderTests = [
-		'https://raw.githubusercontent.com/KhronosGroup/glTF-Sample-Models/master/2.0/CesiumMilkTruck/glTF-Draco/CesiumMilkTruck.gltf',
-		'https://raw.githubusercontent.com/KhronosGroup/glTF-Sample-Models/master/2.0/Box/glTF-Binary/Box.glb',
+		"https://raw.githubusercontent.com/KhronosGroup/glTF-Sample-Models/master/2.0/CesiumMilkTruck/glTF-Draco/CesiumMilkTruck.gltf",
+		"https://raw.githubusercontent.com/KhronosGroup/glTF-Sample-Models/master/2.0/Box/glTF-Binary/Box.glb",
 	];
 
 	const manager = new LoadingManager();
-	const gltfLoader = new GLTFLoader( manager );
-	const dracoLoader = new DRACOLoader( manager );
-	dracoLoader.setDecoderPath( 'https://unpkg.com/three@0.128.0/examples/js/libs/draco/gltf/' );
-	gltfLoader.setDRACOLoader( dracoLoader );
-	manager.addHandler( /\.gltf$/, gltfLoader );
-	manager.addHandler( /\.glb$/, gltfLoader );
+	const gltfLoader = new GLTFLoader(manager);
+	const dracoLoader = new DRACOLoader(manager);
+	dracoLoader.setDecoderPath(
+		"https://unpkg.com/three@0.128.0/examples/js/libs/draco/gltf/"
+	);
+	gltfLoader.setDRACOLoader(dracoLoader);
+	manager.addHandler(/\.gltf$/, gltfLoader);
+	manager.addHandler(/\.glb$/, gltfLoader);
 
-	for ( const url of delegatedLoaderTests ) {
-
-		const loader = new GLTFExtensionLoader( manager );
-		loader.workingPath = loader.workingPathForURL( url );
-		loader.load( url )
-			.then( res => {
-
-				res.scene.position.set( insertPosition += 5, 0, 0 );
-				controls.target.set( insertPosition / 2, 0, 0 );
-				controls.update();
-				console.log( 'custom loader:', { gltf: res, url } );
-				scene.add( res.scene );
-
-			} );
-
+	for (const url of delegatedLoaderTests) {
+		const loader = new GLTFExtensionLoader(manager);
+		loader.workingPath = loader.workingPathForURL(url);
+		loader.load(url).then((res) => {
+			res.scene.position.set((insertPosition += 5), 0, 0);
+			controls.target.set(insertPosition / 2, 0, 0);
+			controls.update();
+			// console.log("custom loader:", { gltf: res, url });
+			res.scene.traverse((node) => {
+				if (node.type === "Mesh") {
+					// console.log("[node]", url, node);
+					node.castShadow = true;
+					node.receiveShadow = true;
+				}
+			});
+			scene.add(res.scene);
+		});
 	}
 
 	onWindowResize();
-	window.addEventListener( 'resize', onWindowResize, false );
-
+	window.addEventListener("resize", onWindowResize, false);
 }
 
 function onWindowResize() {
-
 	camera.aspect = window.innerWidth / window.innerHeight;
-	renderer.setPixelRatio( window.devicePixelRatio );
-	renderer.setSize( window.innerWidth, window.innerHeight );
+	renderer.setPixelRatio(window.devicePixelRatio);
+	renderer.setSize(window.innerWidth, window.innerHeight);
 	camera.updateProjectionMatrix();
-
 }
 
 function animate() {
-
-	requestAnimationFrame( animate );
+	requestAnimationFrame(animate);
 
 	render();
-
 }
 
 function render() {
-
-	renderer.render( scene, camera );
-
+	renderer.render(scene, camera);
 }
